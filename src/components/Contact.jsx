ffbea8340cbee3ba857f6d8d6f49f5e73bc4845e
Julian <juliancoding30@gmail.com>
2023-07-08 01:15:57 -0700

    emailing works

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,39 +1,108 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Button from "./subcomponents/Button";
 import icon1 from "../../public/icons/Twitter Squared.png";
 import icon2 from "../../public/icons/Instagram (1).png";
 import icon3 from "../../public/icons/Facebook (1).png";
 import Image from "next/image";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_ym3lo8g",
+        "template_56xoylw",
+        {
+          from_name: form.name,
+          to_name: "Fernando",
+          from_email: form.email,
+          to_email: "contact@jssmastery.pro",
+          message: form.message,
+        },
+        "aQ6vYj0jDyU7dLrx9"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you for the message! We will get back to you soon.");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+
+          alert("Message did not send. Something went wrong.");
+        }
+      );
+  };
   return (
     <div
       id="contact"
       className="border-t-4 border-black py-8 flex lg:flex-row flex-col-reverse z-10"
     >
       <div className="lg:w-1/2 px-8 z-10">
-        <form className="bg-[#FFCC9D] flex flex-col w-full rounded-lg border-4 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,100)] p-8 z-10">
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="bg-[#FFCC9D] flex flex-col w-full rounded-lg border-4 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,100)] p-8 z-10"
+        >
           <input
             placeholder="Name"
-            type="name"
-            className="p-4 z-10 text-xl ring-none outline-none placeholder:text-xl bg-white placeholder:text-[#3F3F3F] border-4 border-black rounded-xl shadow-[4px_4px_0px_0px_rgba(0,0,0,100)]"
+            name="name"
+            type="text"
             required
+            value={form.name}
+            onChange={handleChange}
+            className="p-4 z-10 text-xl ring-none outline-none placeholder:text-xl bg-white placeholder:text-[#3F3F3F] border-4 border-black rounded-xl shadow-[4px_4px_0px_0px_rgba(0,0,0,100)]"
           />
           <input
             placeholder="Email"
+            name="email"
             type="email"
-            className="p-4 z-10 my-4 text-xl ring-none outline-none placeholder:text-xl bg-white placeholder:text-[#3F3F3F] border-4 border-black rounded-xl shadow-[4px_4px_0px_0px_rgba(0,0,0,100)]"
             required
+            value={form.email}
+            onChange={handleChange}
+            className="p-4 z-10 my-4 text-xl ring-none outline-none placeholder:text-xl bg-white placeholder:text-[#3F3F3F] border-4 border-black rounded-xl shadow-[4px_4px_0px_0px_rgba(0,0,0,100)]"
           />
           <textarea
             placeholder="Message"
+            name="message"
+            type="text"
+            value={form.message}
+            onChange={handleChange}
             className="p-4 z-10 text-xl ring-none outline-none placeholder:text-xl bg-white placeholder:text-[#3F3F3F] border-4 border-black rounded-xl shadow-[4px_4px_0px_0px_rgba(0,0,0,100)]"
             rows={5}
             required
           />
-          <div className="flex mt-4 z-10">
-            <Button text="Send" size="text-[20px]" link="/" />
-          </div>
+          <button type="submit" className="flex mt-4 z-10">
+            <p
+              className={`py-3 z-10 px-12 bg-[#C5FF52] flex justify-center items-center rounded-lg border-2 border-black text-[20px] hover:-translate-y-[2px] active:translate-x-[2px] active:shadow-[4px_4px_0px_0px_rgba(0,0,0,100)] cursor-pointer duration-300 shadow-[4px_4px_0px_0px_rgba(0,0,0,100)] hover:shadow-[6px_6px_0px_0px_rgba(0,0,0,100)]`}
+            >
+              Send
+            </p>
+          </button>
         </form>
       </div>
       <div className="flex flex-col lg:items-end lg:mb-0 mb-8 items-center text-center lg:text-right px-8 lg:w-1/2 z-10">
